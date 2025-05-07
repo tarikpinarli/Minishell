48ccd16649e4f5c719808c3a8bbbce944f3660de
tarikpinarli <pinarlitarik@gmail.com>
2025-05-07 15:44:00 +0300

    builtin echo and exit are completed

--- builtins/builtin_exit.c
+++ builtins/builtin_exit.c
@@ -6,13 +6,13 @@
 /*   By: tpinarli <tpinarli@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 12:48:37 by tpinarli          #+#    #+#             */
-/*   Updated: 2025/05/07 13:40:41 by tpinarli         ###   ########.fr       */
+/*   Updated: 2025/05/07 15:35:56 by tpinarli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	builtin_exit(char **argv, t_command *cmd)
+int	builtin_exit(char **argv, t_command *cmd, int pid_flag)
 {
 	int	arg_count;
 	int	exit_code;
@@ -25,7 +25,8 @@ int	builtin_exit(char **argv, t_command *cmd)
 		printf("exit: too many arguments\n");
 		return (1);
 	}
-	printf("exit\n");
+    if (pid_flag)
+	    printf("exit\n");
 	if (arg_count == 2)
 		exit_code = ft_atoi(argv[1]);
 	else

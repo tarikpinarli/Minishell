0c15125ae23015ceedca3d539a6a211bf693f084
ykadosh <ykadosh@student.hive.fi>
2025-05-21 21:54:09 +0300

    adds a clearer comment regarding some printf() debugging line that should be kept for now within next_token() in the tokenizer

--- parser/tokenizer_utils.c
+++ parser/tokenizer_utils.c
@@ -102,7 +102,10 @@ static t_token	next_token(char **str, int *line_id)
 		return (extract_special_character_token(str, line_id));
 	else if (**str)
 		return (extract_simple_token(str, line_id));
-	printf("Hello from where you were wondering if we get to or not!!!\n\n"); // FIXME: remove this debugging when ready!
+// FIXME: remove this next debugging line when ready! I leave it because if it actually gets printed out once,
+// it might very probably mean that the returned (empty) will be NULL, BUT we should NOT free and exit like
+// the NULL return from this function leads to (in fill_up_tokens()).
+	printf("Hello from where you were wondering if we get to or not!!!\n\n");
 	return (empty);
 }
 

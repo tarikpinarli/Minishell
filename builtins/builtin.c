c8299b7fef1badbcfc54c140be846188e5eeb084
Tarik Pinarli <tpinarli@c1r1p4.hive.fi>
2025-06-02 13:18:19 +0300

    heredoc cleanup added to builtin.c

--- builtins/builtin.c
+++ builtins/builtin.c
@@ -6,7 +6,7 @@
 /*   By: tpinarli <tpinarli@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 13:27:12 by tpinarli          #+#    #+#             */
-/*   Updated: 2025/05/31 18:03:07 by tpinarli         ###   ########.fr       */
+/*   Updated: 2025/06/02 13:07:43 by tpinarli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,10 @@ int execute_builtin(t_command *cmd, int pid_flag, char ***env)
 	else if (!ft_strcmp(cmd->argv[0], "cd"))
 	{
 		if(builtin_cd(cmd->argv, env) == -1)
+		{
+			cleanup_heredocs(cmd);
 			return (-1);
+		}
 	}
 	else if (!ft_strcmp(cmd->argv[0], "export"))
 		return (builtin_export(cmd->argv, pid_flag, env));

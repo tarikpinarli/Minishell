4ceeec3ae1ee767f68a5ae405992a77296010816
conaffetto <167137372+conaffetto@users.noreply.github.com>
2025-06-13 18:42:20 +0300

    cleans up the header file to comply with the norm

--- minishell.h
+++ minishell.h
@@ -51,11 +51,11 @@ typedef enum e_quote_type
 
 typedef enum e_redir_type
 {
-	REDIR_NONE,		// (not a redirection)
-	REDIR_IN,		// <
-	REDIR_OUT,		// >
-	REDIR_APPEND,	// >>
-	REDIR_HEREDOC	// <<
+	REDIR_NONE,
+	REDIR_IN,
+	REDIR_OUT,
+	REDIR_APPEND,
+	REDIR_HEREDOC
 }	t_redir_type;
 
 typedef struct s_redir
@@ -75,10 +75,10 @@ typedef struct s_token
 
 typedef struct s_command
 {
-	char				**argv;		// Command and arguments
-	t_redir				*in_redir;	// Input redirections
-	t_redir				*out_redir;	// Output redirections
-	struct s_command	*next;		// Next command in pipeline
+	char				**argv;
+	t_redir				*in_redir;
+	t_redir				*out_redir;
+	struct s_command	*next;
 }	t_command;
 
 typedef struct s_parse_state
@@ -90,15 +90,15 @@ typedef struct s_parse_state
 	int			i;
 }	t_parse_state;
 
-typedef struct	s_heredoc
+typedef struct s_heredoc
 {
 	int		fd;
 	char	*heredoc_filename;
 	char	*line;
-	t_redir *in;
+	t_redir	*in;
 }	t_heredoc;
 
-typedef struct	s_pipeline
+typedef struct s_pipeline
 {
 	int			pipefd[2];
 	int			*curr_pipefd;
@@ -125,13 +125,17 @@ int			ft_isspace(char c);
 t_command	*parse_tokens(t_token *tokens, char *input, char ***env);
 int			ft_strcmp(const char *s1, const char *s2);
 void		merge_tokens(t_token *tokens, char *input, char ***env);
-t_redir		*create_redir(t_redir_type type, char *filename, t_quote_type quote);
-void		append_redir(t_redir_type type, t_redir *new_redir, t_command *current);
+t_redir		*create_redir(t_redir_type type, char *filename,
+				t_quote_type quote);
+void		append_redir(t_redir_type type, t_redir *new_redir,
+				t_command *current);
 char		**argv_add(char **argv, char *new_arg);
-void		handle_malloc_err(char *input, t_token *tokens,t_command *head, char ***env);
-void		append_argument(t_token *tokens, char *input, t_parse_state *st, char ***env);
-void	init_command_if_needed(char *input, t_token *tokens,
-	t_parse_state *st, char ***env);
+void		handle_malloc_err(char *input, t_token *tokens, t_command *head,
+				char ***env);
+void		append_argument(t_token *tokens, char *input, t_parse_state *st,
+				char ***env);
+void		init_command_if_needed(char *input, t_token *tokens,
+				t_parse_state *st, char ***env);
 
 // expansion functions
 void		expand_tokens(t_token *tokens, char *input, char ***env);
@@ -195,14 +199,16 @@ int			execute_builtin(t_command *cmd, int pid_flag, char ***env);
 int			builtin_pwd(char **argv);
 int			builtin_unset(char **argv, char ***env);
 int			builtin_env(char ***env);
-int			builtin_exit(char **argv, t_command *cmd, int pid_flag, char ***env);
+int			builtin_exit(char **argv, t_command *cmd, int pid_flag,
+				char ***env);
 int			builtin_echo(char **argv);
 char		*get_env_value(char **env, char *key);
 
 // builtin_cd
 int			builtin_cd(char **argv, char ***env);
 int			update_env_var(char ***env, const char *key, const char *value);
-int			update_pwd_vars(char ***env, const char *oldpwd, const char *newpwd);
+int			update_pwd_vars(char ***env, const char *oldpwd,
+				const char *newpwd);
 
 // builtin_export
 int			builtin_export(char **argv, char ***env);
